b24ba63edf634acafd0ece25fc53c0f578449540
dabreadman <43485369+dabreadman@users.noreply.github.com>
2019-11-17 21:56:55 +0000

    Implemented height and printKeysInOrder.

--- src/BST.java
+++ src/BST.java
@@ -8,171 +8,211 @@
  *
  *************************************************************************/
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.NoSuchElementException;
 
 public class BST<Key extends Comparable<Key>, Value> {
-    private Node root;             // root of BST
+	private Node root;             // root of BST
 
-    /**
-     * Private node class.
-     */
-    private class Node {
-        private Key key;           // sorted by key
-        private Value val;         // associated data
-        private Node left, right;  // left and right subtrees
-        private int N;             // number of nodes in subtree
+	/**
+	 * Private node class.
+	 */
+	private class Node {
+		private Key key;           // sorted by key
+		private Value val;         // associated data
+		private Node left, right;  // left and right subtrees
+		private int N;             // number of nodes in subtree
 
-        public Node(Key key, Value val, int N) {
-            this.key = key;
-            this.val = val;
-            this.N = N;
-        }
-    }
+		public Node(Key key, Value val, int N) {
+			this.key = key;
+			this.val = val;
+			this.N = N;
+		}
+	}
 
-    // is the symbol table empty?
-    public boolean isEmpty() { return size() == 0; }
+	// is the symbol table empty?
+	public boolean isEmpty() { return size() == 0; }
 
-    // return number of key-value pairs in BST
-    public int size() { return size(root); }
+	// return number of key-value pairs in BST
+	public int size() { return size(root); }
 
-    // return number of key-value pairs in BST rooted at x
-    private int size(Node x) {
-        if (x == null) return 0;
-        else return x.N;
-    }
+	// return number of key-value pairs in BST rooted at x
+	private int size(Node x) {
+		if (x == null) return 0;
+		else return x.N;
+	}
 
-    /**
-     *  Search BST for given key.
-     *  Does there exist a key-value pair with given key?
-     *
-     *  @param key the search key
-     *  @return true if key is found and false otherwise
-     */
-    public boolean contains(Key key) {
-        return get(key) != null;
-    }
+	/**
+	 *  Search BST for given key.
+	 *  Does there exist a key-value pair with given key?
+	 *
+	 *  @param key the search key
+	 *  @return true if key is found and false otherwise
+	 */
+	public boolean contains(Key key) {
+		return get(key) != null;
+	}
 
-    /**
-     *  Search BST for given key.
-     *  What is the value associated with given key?
-     *
-     *  @param key the search key
-     *  @return value associated with the given key if found, or null if no such key exists.
-     */
-    public Value get(Key key) { return get(root, key); }
+	/**
+	 *  Search BST for given key.
+	 *  What is the value associated with given key?
+	 *
+	 *  @param key the search key
+	 *  @return value associated with the given key if found, or null if no such key exists.
+	 */
+	public Value get(Key key) { return get(root, key); }
 
-    private Value get(Node x, Key key) {
-        if (x == null) return null;
-        int cmp = key.compareTo(x.key);
-        if      (cmp < 0) return get(x.left, key);
-        else if (cmp > 0) return get(x.right, key);
-        else              return x.val;
-    }
+	private Value get(Node x, Key key) {
+		if (x == null) return null;
+		int cmp = key.compareTo(x.key);
+		if      (cmp < 0) return get(x.left, key);
+		else if (cmp > 0) return get(x.right, key);
+		else              return x.val;
+	}
 
-    /**
-     *  Insert key-value pair into BST.
-     *  If key already exists, update with new value.
-     *
-     *  @param key the key to insert
-     *  @param val the value associated with key
-     */
-    public void put(Key key, Value val) {
-        if (val == null) { delete(key); return; }
-        root = put(root, key, val);
-    }
+	/**
+	 *  Insert key-value pair into BST.
+	 *  If key already exists, update with new value.
+	 *
+	 *  @param key the key to insert
+	 *  @param val the value associated with key
+	 */
+	public void put(Key key, Value val) {
+		if (val == null) { delete(key); return; }
+		root = put(root, key, val);
+	}
 
-    private Node put(Node x, Key key, Value val) {
-        if (x == null) return new Node(key, val, 1);
-        int cmp = key.compareTo(x.key);
-        if      (cmp < 0) x.left  = put(x.left,  key, val);
-        else if (cmp > 0) x.right = put(x.right, key, val);
-        else              x.val   = val;
-        x.N = 1 + size(x.left) + size(x.right);
-        return x;
-    }
+	private Node put(Node x, Key key, Value val) {
+		if (x == null) return new Node(key, val, 1);
+		int cmp = key.compareTo(x.key);
+		if      (cmp < 0) x.left  = put(x.left,  key, val);
+		else if (cmp > 0) x.right = put(x.right, key, val);
+		else              x.val   = val;
+		x.N = 1 + size(x.left) + size(x.right);
+		return x;
+	}
 
-    /**
-     * Tree height.
-     *
-     * Asymptotic worst-case running time using Theta notation: TODO
-     *
-     * @return the number of links from the root to the deepest leaf.
-     *
-     * Example 1: for an empty tree this should return -1.
-     * Example 2: for a tree with only one node it should return 0.
-     * Example 3: for the following tree it should return 2.
-     *   B
-     *  / \
-     * A   C
-     *      \
-     *       D
-     */
-    public int height() {
-      //TODO fill in the correct implementation.
-      return -1;
-    }
+	/**
+	 * Tree height.
+	 *
+	 * Asymptotic worst-case running time using Theta notation: TODO
+	 *
+	 * @return the number of links from the root to the deepest leaf.
+	 *
+	 * Example 1: for an empty tree this should return -1.
+	 * Example 2: for a tree with only one node it should return 0.
+	 * Example 3: for the following tree it should return 2.
+	 *   B
+	 *  / \
+	 * A   C
+	 *      \
+	 *       D
+	 */
+	public int height() {
+		//TODO fill in the correct implementation.
+		return height(root);
+	}
 
-    /**
-     * Median key.
-     * If the tree has N keys k1 < k2 < k3 < ... < kN, then their median key 
-     * is the element at position (N+1)/2 (where "/" here is integer division)
-     *
-     * @return the median key, or null if the tree is empty.
-     */
-    public Key median() {
-      if (isEmpty()) return null;
-      //TODO fill in the correct implementation. The running time should be Theta(h), where h is the height of the tree.
-      return null;
-    }
+	private int height(Node node, int current_height) {
+		int hL = current_height;
+		int hR = current_height;
+		if(node.left!=null) hL+=height(node.left,hL) +1;
+		if(node.right!=null) hR+=height(node.right,hR) +1;
+		return (hL>hR? hL:hR);
+
+	}
+
+	private int height(Node node) {
+		if (node == null) return -1;
+		int leftHeight = height(node.left);
+		int rightHeight = height(node.right);
+		return (int) Math.max(leftHeight, rightHeight) + 1;
+	}
+
+	/**
+	 * Median key.
+	 * If the tree has N keys k1 < k2 < k3 < ... < kN, then their median key 
+	 * is the element at position (N+1)/2 (where "/" here is integer division)
+	 *
+	 * @return the median key, or null if the tree is empty.
+	 */
+	public Key median() {
+		if (isEmpty()) return null;
+		//TODO fill in the correct implementation. The running time should be Theta(h), where h is the height of the tree.
+		return null;
+	}
 
 
-    /**
-     * Print all keys of the tree in a sequence, in-order.
-     * That is, for each node, the keys in the left subtree should appear before the key in the node.
-     * Also, for each node, the keys in the right subtree should appear before the key in the node.
-     * For each subtree, its keys should appear within a parenthesis.
-     *
-     * Example 1: Empty tree -- output: "()"
-     * Example 2: Tree containing only "A" -- output: "(()A())"
-     * Example 3: Tree:
-     *   B
-     *  / \
-     * A   C
-     *      \
-     *       D
-     *
-     * output: "((()A())B(()C(()D())))"
-     *
-     * output of example in the assignment: (((()A(()C()))E((()H(()M()))R()))S(()X()))
-     *
-     * @return a String with all keys in the tree, in order, parenthesized.
-     */
-    public String printKeysInOrder() {
-      if (isEmpty()) return "()";
-      // TODO fill in the correct implementation
-      return null;
-    }
-    
-    /**
-     * Pretty Printing the tree. Each node is on one line -- see assignment for details.
-     *
-     * @return a multi-line string with the pretty ascii picture of the tree.
-     */
-    public String prettyPrintKeys() {
-      //TODO fill in the correct implementation.
-      return null;
-    }
+	/**
+	 * Print all keys of the tree in a sequence, in-order.
+	 * That is, for each node, the keys in the left subtree should appear before the key in the node.
+	 * Also, for each node, the keys in the right subtree should appear before the key in the node.
+	 * For each subtree, its keys should appear within a parenthesis.
+	 *
+	 * Example 1: Empty tree -- output: "()"
+	 * Example 2: Tree containing only "A" -- output: "(()A())"
+	 * Example 3: Tree:
+	 *   B
+	 *  / \
+	 * A   C
+	 *       D
+	 *
+	 * output: "((()A())B(()C(()D())))"
+	 *
+	 * output of example in the assignment: (((()A(()C()))E((()H(()M()))R()))S(()X()))
+	 *
+	 * @return a String with all keys in the tree, in order, parenthesized.
+	 */
+	public String printKeysInOrder() {
 
-    /**
-     * Deteles a key from a tree (if the key is in the tree).
-     * Note that this method works symmetrically from the Hibbard deletion:
-     * If the node to be deleted has two child nodes, then it needs to be
-     * replaced with its predecessor (not its successor) node.
-     *
-     * @param key the key to delete
-     */
-    public void delete(Key key) {
-      //TODO fill in the correct implementation.
-    }
+		return printKeysInOrder(root);
+	}
+
+	private String printKeysInOrder(Node n) {
+		if (n==null) return "()";
+		String l ="(";
+		String r = ")";
+		l += printKeysInOrder(n.left);
+		r = printKeysInOrder(n.right)+r;
+		return (String) (l+n.key+r);
+	}
+
+	/**
+	 * Pretty Printing the tree. Each node is on one line -- see assignment for details.
+	 *
+	 * @return a multi-line string with the pretty ascii picture of the tree.
+	 */
+	public String prettyPrintKeys() {
+		//TODO fill in the correct implementation.
+		return null;
+	}
+
+	/**
+	 * Deteles a key from a tree (if the key is in the tree).
+	 * Note that this method works symmetrically from the Hibbard deletion:
+	 * If the node to be deleted has two child nodes, then it needs to be
+	 * replaced with its predecessor (not its successor) node.
+	 *
+	 * @param key the key to delete
+	 */
+	public void delete(Key key) {
+		//TODO fill in the correct implementation.
+	}
+
+	public static void main(String[]args) {
+		BST<Integer, Integer> bst = new BST<Integer, Integer>();     
+		bst.put(7, 7);   //        _7_
+		bst.put(8, 8);   //      /     \
+		bst.put(3, 3);   //    _3_      8
+		bst.put(1, 1);   //  /     \
+		bst.put(2, 2);   // 1       6
+		bst.put(6, 6);   //  \     /
+		bst.put(4, 4);   //   2   4
+		bst.put(5, 5);   //        \
+						 //         5
+		System.out.println(bst.printKeysInOrder());
+		System.out.println("(((()1(()2()))3((()4(()5()))6()))7(()8()))");
+	}
 
 }
